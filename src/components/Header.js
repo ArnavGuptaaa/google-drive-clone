a5ae1533bd3e296ead657ba672eac386cd960b23
Arnav Gupta <arnavrgupta002@gmail.com>
2021-10-13 09:54:52 +0530

    Added hover and active states to sidebar.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,40 @@
 import { Avatar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
+import Button from '@material-ui/core/Button';
+import { useHistory } from 'react-router';
 
 function Header() {
-	// Avatar Styles
-	const useStyles = makeStyles({
+	const useStyles = makeStyles((theme) => ({
+		margin: {
+			margin: 10,
+		},
 		avatar: {
 			backgroundColor: 'red',
 		},
-	});
+	}));
+	const history = useHistory();
+
+	const handleLogout = () => {
+		// Logout Post Request
+		fetch('http://localhost:5000/login', {
+			method: 'POST',
+			withCredentials: true,
+			credentials: 'include',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				if (data.sucess) {
+					history.push('/login');
+				} else {
+					console.log('Hello');
+				}
+			})
+			.catch((err) => console.log(err));
+	};
 
 	const classes = useStyles();
 	return (
@@ -21,7 +48,16 @@ function Header() {
 			</div>
 
 			<div className="avatar">
-				<a href="/logout">LOGOUT</a>
+				<Button
+					type="submit"
+					variant="contained"
+					size="small"
+					color="primary"
+					className={classes.margin}
+					// onClick={validateCreds}
+				>
+					Logout
+				</Button>
 				<Avatar className={classes.avatar}>SA</Avatar>
 			</div>
 		</div>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,8 +3,12 @@ import GroupIcon from '@material-ui/icons/Group';
 import ComputerIcon from '@material-ui/icons/Computer';
 import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
 
 const SideBar = ({ sideBarOption, setSideBarOption }) => {
+	const [listActive1, setListActive1] = useState('list-item-active');
+	const [listActive2, setListActive2] = useState('');
+
 	// Button Styles
 	const useStyles = makeStyles({
 		btn: {
@@ -31,18 +35,22 @@ const SideBar = ({ sideBarOption, setSideBarOption }) => {
 
 			<ul className="sidebar-list">
 				<li
-					className="list-item"
+					className={`list-item ${listActive1}`}
 					onClick={() => {
 						handleClick(0);
+						setListActive1('list-item-active');
+						setListActive2('');
 					}}
 				>
 					<ComputerIcon className={classes.btn} fontSize="large" />
 					<p className="list-text">My Drive</p>
 				</li>
 				<li
-					className="list-item"
+					className={`list-item ${listActive2}`}
 					onClick={() => {
 						handleClick(1);
+						setListActive2('list-item-active');
+						setListActive1('');
 					}}
 				>
 					<GroupIcon className={classes.btn} fontSize="large" />
